5e61110f7380221f97b33b6d84b87c42336cf562
busyboyent <55663134+busyboyent@users.noreply.github.com>
2020-06-04 16:14:14 +0500

    add new method

--- scaner task3/scaner.py
+++ scaner task3/scaner.py
@@ -1,25 +1,45 @@
 import socket
+import threading
+import psutil
+from random import randint
 
-def scan_port(ip,port):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.settimeout(0.5)
-  try:
-     connect = sock.connect((ip,port))
-     print('Port :',port,' its open.')
-     connect.close()
-  except:
-     pass
+
+def getfreeport():
+    port = randint(49152,65535)
+    portsinuse=[]
+    while True:
+        conns = psutil.net_connections()
+        for conn in conns:
+            portsinuse.append(conn.laddr[1])
+        if port in portsinuse:
+            port = randint(49152,65535)
+        else:
+            break
+    return port
+
+#def scan_port(ip, port):
+#  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##  sock.settimeout(0.5)
+##  try:
+#    connect = sock.connect((ip,port))
+#     print('Port :',port,' its open.')
+#     connect.close()
+#  except:
+#     pass
 
 def get_open_port():
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("",0))
-        s.listen(1)
-        port = s.getsockname()[1]
-        s.close()
-        return port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("",0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
-ip = input()
-print('введите ip')
-for i in range(1000):
-  scan_port(ip,i)
+ip = 'введите ip'
+
+
+answer = get_open_port()
+print(answer)
+
+print(getfreeport())
